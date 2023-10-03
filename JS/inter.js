9772eeea0ac58bd1b8377301d88c5c4c7af26e57
picorelli1979 <fabricio001devenloper@gmail.com>
2023-10-02 21:05:33 -0300

    MEU PRIMEIRO COMMIT PELO GIT AQUI CODIGO git add .!

---
+++ JS/inter.js
@@ -0,0 +1,26 @@
+/* AQUI ESTÃO OS LINKS DAS REDES SOCIAIS*/
+
+function Carregar_Face(){
+
+    window.location.href = 'https://www.facebook.com/profile.php?id=100007235895211' 
+}
+
+function Carregar_Insta(){
+
+   window.location.href = 'https://www.instagram.com/fabricio_paim001/'
+}
+
+function Carregar_Twiter(){
+
+    window.location.href = 'https://twitter.com/FabricioCatel'
+ }
+
+function Carregar_Linkedin(){
+
+    window.location.href = 'https://www.linkedin.com/in/fabricio-catel-569374225/'
+}
+
+function Carregar_Guithub(){
+
+    window.location.href = 'https://github.com/picorelli1979'
+} 
